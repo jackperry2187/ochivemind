aa7a3fab120711ef7e5840737c5fc547ea171cff
Jackson Perry <jackperry2187@gmail.com>
2023-03-11 23:06:31 -0500

    Adding Pioneer RCQs

--- src/events/rcq.tsx
+++ src/events/rcq.tsx
@@ -19,4 +19,22 @@ export const rcqEvents: Event[] = [
         name: '2-Slot Modern Regional Championship Qualifier',
         link: 'https://alternateu.com/special-events/',
     },
+    {
+        format: 'Pioneer',
+        type: 'RCQ',
+        date: '2023-04-01',
+        time: '09:00am',
+        location: 'Family Fun Hobbies',
+        name: '1-Slot Pioneer Regional Championship Qualifier',
+        link: 'https://www.shopfamilyfunhobbies.com/magic-the-gathering-regional-championship-qualifie.html',
+    },
+    {
+        format: 'Pioneer',
+        type: 'RCQ',
+        date: '2023-04-01',
+        time: '12:00pm',
+        location: 'New World Manga',
+        name: '2-Slot Pioneer Regional Championship Qualifier',
+        link: 'https://locator.wizards.com/events/5009225/false',
+    }
 ]
