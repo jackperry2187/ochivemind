5191d17d03b4987171edc50d7915a0cbf679ae87
Jackson Perry <jackperry2187@gmail.com>
2023-03-19 00:34:52 -0400

    Added separate other file, plus more rcqs

--- src/events/index.tsx
+++ src/events/index.tsx
@@ -1,5 +1,6 @@
 import { mtgoEvents } from "./mtgo";
 import { mtgoWeeklyEvents } from "./mtgoWeekly";
+import { otherEvents } from "./other";
 import { rcqEvents } from "./rcq";
 import { Event } from "./types";
 import { weeklyEvents } from "./weekly";
@@ -9,4 +10,5 @@ export const events: Event[] = [
     ...mtgoWeeklyEvents,
     ...rcqEvents,
     ...weeklyEvents,
+    ...otherEvents,
 ];
---
+++ src/events/other.tsx
@@ -0,0 +1,31 @@
+import { Event } from "./types";
+
+export const otherEvents: Event[] = [
+    {
+        format: 'Pioneer',
+        type: 'OTHER',
+        date: '2023-03-28',
+        time: '6:30pm',
+        location: 'Nova Games',
+        name: 'Pioneer Store Championship @ Nova Games',
+        link: 'https://www.facebook.com/NovaGamesBrick/',
+    },
+    {
+        format: 'Modern',
+        type: 'OTHER',
+        date: '2023-04-14',
+        time: '12:00pm',
+        location: 'Greater Richmond Convention Center',
+        name: 'SCG CON Richmond - Modern 20K, 5K, ReCQ',
+        link: 'https://scgcon.starcitygames.com/event/scg-con-richmond-2023/',
+    },
+    {
+        format: 'Pioneer',
+        type: 'OTHER',
+        date: '2023-04-14',
+        time: '12:00pm',
+        location: 'Greater Richmond Convention Center',
+        name: 'SCG CON Richmond - Pioneer ReCQs',
+        link: 'https://scgcon.starcitygames.com/event/scg-con-richmond-2023/',
+    }
+]
